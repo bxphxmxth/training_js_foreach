ba23621e2c54bd7c245e313d224c5f326561b244
bxphxmxth <fushs@hotmail.fr>
2021-05-14 10:06:07 +0200

    de

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+// -  # Exo 1 Boucles FOREACH
+//     ## - Creer une variable de type array et inserez toutes les personnes de la classe
+//     ## - A l'aide d'une boucle foreach, dites bonjour + prenom à chaque personne du tableau
+let classe = ["Adil","Agim","Ali","Antoine","Chris","Elvis","Fanny","Haroune","Ilias D","Ilias El","Jean","Kevin","Lira","Mouna","Nasila","Nathan","Seif","Stan","Van Hoa","Yassine"];
+
+classe.forEach(eleve =>{
+
+    console.log(`Bonjour ${eleve}`);
+
+
+});
+
+
+// - # Exo 2 Boucle FOREACH
+//     - ## Créez un un array qui contient 5 éléments
+//     - ## Affichez touts les éléments a l'aide d'une boucle foreach
+
+
+
+
+
+//     ## Triez le contenu du tableau donnees en verifiant le type de chaque donnée
+//     ## Creez 4 Tableaux : 'typeString' , 'typeNumber' , 'typeObject' , 'typeAutre.
+//     ## Videz le tableau "donnees" en redistribuant avec l'aide de condition et d'une function prédéfinie trouvée sur internet dans les bons tableaux.
+let donnees = [14, 7, 97, {}, "247", 67, 101, true, 34, 78, [], 'coding school', 0, 11, 32, "italie", 61, null, 'cent', 100];
+
+
+    //1. je console log le type de chacun de mes éléments
+
+    //2. je trie mes éléments en fonction de leur type
+  
+//3. supprime tout ce qu'il y a dans le tableau
+
+//4. j'affiche tout
